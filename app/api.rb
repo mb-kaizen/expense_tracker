5a0d74a6181d51c4881e20fea9e75c8b1b16017a
MB <michael.entr@gmail.com>
2020-04-07 11:46:58 +0900

    initialized ledger

--- app/api.rb
+++ app/api.rb
@@ -6,6 +6,9 @@ module ExpenseTracker
     class API < Sinatra::Base 
         post '/expenses' do
             # JSON.generate('expense_id' => 42)
+            # status 404
+            @ledger = Ledger.new
+
             expense = JSON.parse(request.body.read)
             result = @ledger.record(expense)
             JSON.generate('expense_id' => result.expense_id)
--- app/ledger.rb
+++ app/ledger.rb
@@ -2,5 +2,12 @@ module ExpenseTracker
     RecordResult = Struct.new(:success?, :expense_id, :error_message)
 
     class Ledger
+        def initialize
+            @ledger = Struct.new(:id)
+        end
+
+        def record(expense)
+            
+        end
     end
 end 
